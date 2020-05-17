38394cd9e75611d7f3e57de13fcba0972c003319
shirlybittan <45658190+shirlybittan@users.noreply.github.com>
2020-05-17 23:24:16 +0300

    removing comments

--- testRegex.py
+++ testRegex.py
@@ -6,24 +6,21 @@ import filecmp
 
 class Test_TestRegex(unittest.TestCase):
 
-    #deleting the result Text file created by those tests at the end of all the tests
-  #  def tearDownClass():
-   #     os.remove(UnitTest_txt_path+"/unitTest_Results.txt")
-      #created before every Test
+
     def setUp (self):
         open(os.path.join('resultsFound','Results.txt'),'w').close()
- #
+ 
 
- #   def test_AWS(self):
- #
- #       with open( "scannedFile/unitTest_Regex.txt", "r",errors='ignore') as myFile:
- #           fin = myFile.read()
- #           secretsFinder.AWS_search(
- #                   "resultsFound",
- #                   fin, 
- #                  "scannedFile")
- #       self.assertTrue(filecmp.cmp("resultsFound/Results.txt","expectedResults/result_AWS_true.txt"),
- #                                                               'The files are not equals')
+    def test_AWS(self):
+ 
+        with open( "scannedFile/unitTest_Regex.txt", "r",errors='ignore') as myFile:
+            fin = myFile.read()
+            secretsFinder.AWS_search(
+                    "resultsFound",
+                    fin, 
+                   "scannedFile")
+        self.assertTrue(filecmp.cmp("resultsFound/Results.txt","expectedResults/result_AWS_true.txt"),
+                                                                'The files are not equals')
 
     def test_RSA(self):
 
@@ -37,11 +34,6 @@ class Test_TestRegex(unittest.TestCase):
                                                                 'The files are not equals')
 
 
-
-   #def test_AWS(self):
-   #    self.assertTrue(filecmp.cmp(PythonApplication1.AWS_search(UnitTest_txt_path+"/unitTest_Results_AWS.txt",
-   #                                                              unitTest_file,os.path.join(UnitTest_txt_path,"unitTest_test.txt"),
-    #                                                             ResultsAWSTrueToCompare, 'The files are not equals')))
 
 if __name__ == '__main__':
     unittest.main()
